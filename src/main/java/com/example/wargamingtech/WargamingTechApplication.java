7ae686a97ce995c856866880bdd00c374b13e8dd
nkostenko <nikita.kostenk@emergn.com>
2021-11-20 23:36:29 +0300

    Initial commit

---
+++ src/main/java/com/example/wargamingtech/WargamingTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.wargamingtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WargamingTechApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(WargamingTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wargamingtech/WargamingTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wargamingtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WargamingTechApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
